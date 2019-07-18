bde9c75614e99450f241f32d3931516305f22c55
p3la0 <js.diazn@gmail.com>
2019-07-18 11:33:06 -0400

    agregando autenticacion de google

--- server/config/config.js
+++ server/config/config.js
@@ -25,3 +25,8 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 //seed de token
 
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+//google clientID
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '937759421996-lcq878vkdt84dukpfss7ortjk49knls1.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('./config/config');
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
-
+const path = require('path');
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-
+//habilitar index
+app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
